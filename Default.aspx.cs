4e5a2859a7ef57d8ddb04924ec4b06c329ae1f7a
Bindiya Rana <bindiya02rana@gmail.com>
2016-01-30 16:59:00 -0500

    Create Default.aspx.cs
    this prg will show u how to create textbox which will get value as string and display into label... 
    (user enter value into text box click button and thn display into label )

---
+++ Default.aspx.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string a = TextBox1.Text; //this will store text value into string variable 
+        Label1.Text = a; // label with get string value and display 
+
+    }
+}
